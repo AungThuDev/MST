d7eae0b448092bbf8cd6c55097b074d9941852e7
thetnaingaung-BWD <thetnaingaung2333@gmail.com>
2026-07-14 11:28:33 +0630

    update database seeder to include campus and info entries; rename package to MST

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\CampusContent;
 use App\Models\Faq;
+use App\Models\Info;
+use App\Models\Phone;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
@@ -23,6 +26,30 @@ class DatabaseSeeder extends Seeder
             'name' => 'userone',
             'email' => 'userone@gmail.com',
             'password' => Hash::make('password')
+        ]);
+        CampusContent::create([
+            'name' => 'MST Campus',
+            'address' => 'No. 36, Alan Pya Pagoda Street, Yangon, Myanmar',
+        ]);
+        Phone::create([
+            'campus_id' => 1,
+            'number' => '+95 9 422288106',
+        ]);
+        Info::create([
+            'name' => 'email',
+            'link' => 'mst@gmail.com'
+        ]);
+        Info::create([
+            'name' => 'facebook',
+            'link' => 'https://www.facebook.com/mstcampus'
+        ]);
+        Info::create([
+            'name' => 'youtube',
+            'link' => 'https://www.youtube.com/mstcampus'
+        ]);
+        Info::create([
+            'name' => 'linkedin',
+            'link' => 'https://www.linkedin.com/school/mstcampus'
         ]);
     }
 }
